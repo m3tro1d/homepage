b7d93af9aa43b790b99e42222e5ead01fe0e4a1c
m3tro1d <qazwf734@gmail.com>
2020-03-03 21:05:23 +0300

    Render index view in controller

--- app_server/controllers/index.js
+++ app_server/controllers/index.js
@@ -1,6 +1,9 @@
 // Index page
 module.exports.index = (req, res, next) => {
-  res.end('Index page.');
+  res.render('index', {
+    title: 'm3tro1d\'s homepage',
+    page_title: 'm3tro1d.'
+  });
 }
 
 module.exports.software = (req, res, next) => {
