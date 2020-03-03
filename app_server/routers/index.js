027bb1df8a4b92bb8727553a131b90f5d57ea642
m3tro1d <qazwf734@gmail.com>
2020-03-03 20:43:13 +0300

    Add placeholder for software route

--- app_server/routers/index.js
+++ app_server/routers/index.js
@@ -8,4 +8,9 @@ router.get('/', (req, res, next) => {
   res.end('Index page.');
 });
 
+// Software info page
+router.get('/software', (req, res, next) => {
+  res.end('Software page.');
+});
+
 module.exports = router;
