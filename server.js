ff630d3dd7f092bca35c32afa35e7880208618eb
m3tro1d <qazwf734@gmail.com>
2020-03-04 19:19:29 +0300

    Change server port

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.use((err, req, res, next) => {
   res.end(err.message);
 });
 
-app.listen(1337, () => console.log('Server listening on port 1337.'));
+app.listen(80, () => console.log('Server listening on port 80.'));
