5a8521110811dcac629bdfd0e50e0c4e3d3951bb
m3tro1d <qazwf734@gmail.com>
2020-03-09 11:58:00 +0300

    Use blog controller in router

--- app_server/routers/blog.js
+++ app_server/routers/blog.js
@@ -1,15 +1,13 @@
 const express = require('express');
 
+const ctrlBlog = require('../controllers/blog');
+
 // Router for client-side blog routes
 const router = express.Router();
 
 // Blog index page
-router.get('/', (req, res, next) => {
-  res.end('Blog index page.');
-});
+router.get('/', ctrlBlog.index);
 // Post page
-router.get('/:posturl', (req, res, next) => {
-  res.end('Post page.');
-});
+router.get('/:posturl', ctrlBlog.post);
 
 module.exports = router;
