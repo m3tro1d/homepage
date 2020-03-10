c98e36ceddad133f90415676dd76e1262f58df72
m3tro1d <qazwf734@gmail.com>
2020-03-10 20:46:14 +0300

    Use auth middleware in blog API

--- app_api/routers/blog.js
+++ app_api/routers/blog.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const ctrlBlog = require('../controllers/blog');
+const auth = require('../../middleware/auth');
 
 // Router for the blog API routes
 const router = express.Router();
@@ -10,10 +11,10 @@ router.get('/', ctrlBlog.getSeveralPosts);
 // Get certain post
 router.get('/:posturl', ctrlBlog.getOnePost);
 // Create a post
-router.post('/', ctrlBlog.createPost);
+router.post('/', auth, ctrlBlog.createPost);
 // Update a post
-router.put('/:posturl', ctrlBlog.updatePost);
+router.put('/:posturl', auth, ctrlBlog.updatePost);
 // Delete a post
-router.delete('/:posturl', ctrlBlog.deletePost);
+router.delete('/:posturl', auth, ctrlBlog.deletePost);
 
 module.exports = router;
