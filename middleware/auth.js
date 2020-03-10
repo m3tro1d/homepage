2b0695e391bb60da14d3a0ed7d8fc565895a3a39
m3tro1d <qazwf734@gmail.com>
2020-03-10 20:44:53 +0300

    Add authorizing middleware

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+// Middleware function for authorizing access to API
+module.exports = (req, res, next) => {
+  // Check for password match
+  if (req.body.api_pass = process.env.API_PASS) {
+    next();
+  } else {
+    sendJsonResponse(res, 401, { message: 'Unauthorized.' });
+  }
+}
+
+
+// Useful functions
+// Ends res with given status and json content
+function sendJsonResponse(res, status, content) {
+  res.status(status);
+  res.json(content);
+}
