06d46bc1f10fe406babeb32f3309ba4e5598f85e
m3tro1d <qazwf734@gmail.com>
2021-01-30 14:20:00 +0300

    Add a renderer placeholder

--- app_api/controllers/renderer.js
+++ app_api/controllers/renderer.js
@@ -1,3 +1,19 @@
+const mathjax = require('mathjax');
+
 module.exports.render = (req, res, next) => {
-  // Render the input and send it
+  mathjax.init({
+    loader: {load: ['input/tex', 'output/svg']}
+  }).then((MathJax) => {
+      const svg = MathJax.tex2svg('\\frac{1}{2}', {display: true});
+      console.log(MathJax.startup.adaptor.outerHTML(svg));
+  }).catch((err) => console.log(err.message));
+  sendJsonResponse(res, 200, {message: 'Success'})
+}
+
+
+// Useful functions
+// Ends res with given status and json content
+function sendJsonResponse(res, status, content) {
+  res.status(status);
+  res.json(content);
 }
