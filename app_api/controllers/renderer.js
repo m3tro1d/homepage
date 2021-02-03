f0ce7cc89d6a3c07d8a8b10630a39c34df523c91
m3tro1d <qazwf734@gmail.com>
2021-02-03 17:10:40 +0300

    Some fixes in the renderer controller

--- app_api/controllers/renderer.js
+++ app_api/controllers/renderer.js
@@ -2,14 +2,21 @@ const mathjax = require('mathjax');
 
 module.exports.render = (req, res, next) => {
   // Initialize the renderer
-  mathjax.init( {loader: {load: ['input/tex', 'output/svg']}} )
+  mathjax
+    .init({
+      loader: {
+        load: ['input/tex', 'output/svg']
+      }
+    })
     .then((MathJax) => {
     // Render and send the image
     const svg = MathJax.tex2svg(req.body.input, {display: true});
-    sendJsonResponse(res, 201, {output: MathJax.startup.adaptor.outerHTML(svg)})
-  }).catch((err) => {
+    sendJsonResponse(res, 201, {
+      output: MathJax.startup.adaptor.outerHTML(svg)
+    });
+  }).catch(err => {
     // Check for errors
-    sendJsonResponse(res, 400, {message: err.message});
+    sendJsonResponse(res, 400, err);
   });
 }
 
